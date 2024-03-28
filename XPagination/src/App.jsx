f8886402e42dd4d54600e37a06372ea223531be2
Rahul9214 <147570570+Rahul9214@users.noreply.github.com>
2024-03-28 19:48:51 +0530

    latest commit

--- XPagination/src/App.jsx
+++ XPagination/src/App.jsx
@@ -13,7 +13,12 @@ function EmployeeTable() {
 
   const fetchEmployeeData = () => {
     fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json")
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
       .then((data) => {
         const totalEmployees = data.length;
         const totalPages = Math.ceil(totalEmployees / 10);
